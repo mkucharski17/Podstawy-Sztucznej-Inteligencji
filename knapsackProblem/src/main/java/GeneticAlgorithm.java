313dd0ff366ecce82205f8ca150609b2033245ee
Michał <m.kucharski17@wp.pl>
2020-03-21 21:15:08 +0100

    add uniform crossover

--- knapsackProblem/src/main/java/GeneticAlgorithm.java
+++ knapsackProblem/src/main/java/GeneticAlgorithm.java
@@ -23,16 +23,31 @@ public class GeneticAlgorithm {
         //TODO
     }
 
-    private static boolean[] cross(boolean[] mother, boolean[] father) {
+    private static boolean[] singlePointCrossover(boolean[] mother, boolean[] father) {
         boolean[] child = new boolean[mother.length];
         int divisionPoint = new Random().nextInt((mother.length));
-        System.out.println(divisionPoint);
 
         System.arraycopy(mother, 0, child, 0, divisionPoint);
         System.arraycopy(father, divisionPoint + 1, child, divisionPoint + 1, father.length - divisionPoint - 1);
 
         return child;
     }
+
+    public static boolean[] uniformCrossover(boolean[] mother, boolean[] father) {
+        boolean[] child = new boolean[mother.length];
+        for (int i = 0 ; i < child.length ; i++){
+            boolean geneOrigin = new Random().nextBoolean();
+
+            if(geneOrigin)
+                child[i] = father[i];
+            else
+                child[i] = mother[i];
+        }
+
+        return child;
+    }
+
+
 
     private static boolean[] mutate(boolean[] genotype) {
         int mutatedGene = new Random().nextInt((genotype.length));
